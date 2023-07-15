c63dea883a373cb38dfee412c1a71a28443a5168
SaadMeer <saad.wicks@gmail.com>
2023-07-15 23:45:32 +0500

    Addproduct dashboard

--- vendor/js/app.js
+++ vendor/js/app.js
@@ -33,17 +33,90 @@
 //     var location = document.getElementById('search-location').value;
 //     var nearby = document.getElementById('nearby-options').value;
 
-    // Perform search logic and display results
-    // You can use AJAX or fetch to send the search parameters to the server and receive the results
-    // For simplicity, let's just display the search parameters as an example
+// Perform search logic and display results
+// You can use AJAX or fetch to send the search parameters to the server and receive the results
+// For simplicity, let's just display the search parameters as an example
 //     document.getElementById('search-results').innerHTML = `
 //       <p>Search by business: ${business}</p>
 //       <p>Search by location: ${location}</p>
 //       <p>What's Nearby: ${nearby}</p>
 //     `;
 // }
-
 /* Search Filter End */
+/* addLocationProduct Start */
+// function addLocationProducttoggleDropdown() {
+//   var dropdownToggle = document.querySelector('.addLocationProduct-toggle');
+//   dropdownToggle.classList.toggle('open');
+// }
+function addLocationProducttoggleDropdown() {
+  var dropdownToggle = document.querySelector('.addLocationProduct-toggle');
+  dropdownToggle.classList.toggle('open');
+}
+
+var dropdownOptions = document.querySelectorAll('.addLocationProduct-option');
+
+dropdownOptions.forEach(function (option) {
+  option.addEventListener('click', function () {
+    var selectedOptionText = option.textContent;
+    var dropdownToggle = document.querySelector('.addLocationProduct-toggle');
+    dropdownToggle.textContent = selectedOptionText;
+    dropdownToggle.classList.remove('open');
+    dropdownToggle.insertAdjacentHTML('beforeend', '<span class="addLocationProduct-arrow"></span>');
+  });
+});
+/* addLocationProduct End */
+// 
+/* AddLocation Producr Dashboard */
+function selectAllProducts() {
+  const productRadios = document.getElementsByName("product");
+  const selectAllRadio = document.getElementsByName("selectAll")[0];
+  
+  productRadios.forEach(function(radio) {
+    radio.checked = selectAllRadio.checked;
+  });
+}
+
+function editRow(button) {
+  const row = button.closest("tr");
+  const dataFields = row.getElementsByClassName("data");
+  const editFields = row.getElementsByClassName("edit-field");
+  const editButton = row.getElementsByClassName("edit-button")[0];
+  const saveButton = row.getElementsByClassName("save-button")[0];
+
+  for (let i = 0; i < dataFields.length; i++) {
+    dataFields[i].style.display = "none";
+    editFields[i].style.display = "inline-block";
+    editFields[i].value = dataFields[i].textContent;
+  }
+  
+  editButton.style.display = "none";
+  saveButton.style.display = "inline-block";
+}
+
+function saveRow(button) {
+  const row = button.closest("tr");
+  const dataFields = row.getElementsByClassName("data");
+  const editFields = row.getElementsByClassName("edit-field");
+  const editButton = row.getElementsByClassName("edit-button")[0];
+  const saveButton = row.getElementsByClassName("save-button")[0];
+
+  for (let i = 0; i < dataFields.length; i++) {
+    dataFields[i].textContent = editFields[i].value;
+    dataFields[i].style.display = "inline-block";
+    editFields[i].style.display = "none";
+  }
+  
+  editButton.style.display = "inline-block";
+  saveButton.style.display = "none";
+}
+
+function removeProduct(button) {
+  const row = button.closest("tr");
+  row.parentNode.removeChild(row);
+}
+/* AddLocation Producr Dashboard End */
+
+
 /* Login Start */
 const loginButton = document.getElementById("loginButton");
 const container = document.getElementById("container");
@@ -76,3 +149,4 @@ loginCross.addEventListener("click", (event) => {
   }
 });
 /* Login End */
+
