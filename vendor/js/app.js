60bc3f01a7e4e7523fcf84dcf3e062e2cab60497
SaadMeer <saad.wicks@gmail.com>
2023-07-21 23:48:12 +0500

    profile Location Page Update

--- vendor/js/app.js
+++ vendor/js/app.js
@@ -1,3 +1,21 @@
+/* Profile_Location_Tabs Start */
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+/* Profile_Location_Tabs End */
+
+
 /* DashboardSetting Checkbox Start */
 var checkboxes = document.querySelectorAll('.checkbox');
 checkboxes.forEach(function (checkbox) {
