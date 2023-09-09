bb97f4a2a5588199737d1a7be6e3dc38d46d18e3
SaadMeer <saad.wicks@gmail.com>
2023-09-09 13:18:39 +0500

    carousel added

--- vendor/js/carousel.js
+++ vendor/js/carousel.js
@@ -1,30 +1,53 @@
-const carousel = document.querySelector('.carousel');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-
-const cardWidth = 110;
-
-let currentIndex = 2;
-
-prevBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + 5) % 5;
-  updateCarousel();
-});
-
-nextBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % 5;
-  updateCarousel();
-});
-
-function updateCarousel() {
-  const translateX = -(currentIndex - 2) * cardWidth;
-  carousel.style.transform = `translateX(${translateX}px)`;
-
-  const cards = document.querySelectorAll('.carousel-card');
-  cards.forEach((card, index) => {
-    card.classList.toggle('active-card', index === currentIndex);
+$(document).ready(function () {
+  // Initialize Flickity with your options
+  var elem = document.querySelector('.carousel');
+  var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'center', // Center the selected cell
+    pageDots: false,
+    groupCells: '20%',
+    selectedAttraction: 0.03,
+    friction: 0.15,
+    wrapAround: true, // Enable wrap-around behavior
   });
-}
 
-updateCarousel();
+  // Select the second carousel-cell
+  flkty.select(1); // Index 1 represents the second cell (JavaScript index starts from 0).
+
+  // Handle click events for the "Call" button
+  $(".b").click(function () {
+    $(this).toggleClass("b");
+    $(this).toggleClass("b-selected");
+  });
+});
+
+// const carousel = document.querySelector('.carousel');
+// const prevBtn = document.querySelector('.prev-btn');
+// const nextBtn = document.querySelector('.next-btn');
+
+// const cardWidth = 110;
+
+// let currentIndex = 2;
+
+// prevBtn.addEventListener('click', () => {
+//   currentIndex = (currentIndex - 1 + 5) % 5;
+//   updateCarousel();
+// });
+
+// nextBtn.addEventListener('click', () => {
+//   currentIndex = (currentIndex + 1) % 5;
+//   updateCarousel();
+// });
+
+// function updateCarousel() {
+//   const translateX = -(currentIndex - 2) * cardWidth;
+//   carousel.style.transform = `translateX(${translateX}px)`;
+
+//   const cards = document.querySelectorAll('.carousel-card');
+//   cards.forEach((card, index) => {
+//     card.classList.toggle('active-card', index === currentIndex);
+//   });
+// }
+
+// updateCarousel();
 
